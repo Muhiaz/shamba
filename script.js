9a3ecac289fdc194f8687afe4a90ba8deb57ac5e
muhiaz <davidmuhia87@gmail.com>
2022-01-24 18:51:20 +0300

    Edited the landing page

--- script.js
+++ script.js
@@ -28,3 +28,53 @@ function changeSlide(){
 	slider[index].classList.add('active')
 }
 let autoplay = setInterval(nextSlide,5000);
+
+
+const homeslider = document.querySelector('.homeslider').children;
+console.log(homeslider);
+let homeslideindex = 0;
+function nexthomeSlide(){
+	if(homeslideindex == homeslider.length - 1){
+		homeslideindex = 0;
+	}
+	else{
+		homeslideindex++;
+	}
+	changehomeSlider();
+	updateCircle();
+}
+	function changehomeSlider(){
+		for(let j=0; j<homeslider.length; j++){
+			homeslider[j].classList.remove('active');
+		}
+		homeslider[homeslideindex].classList.add('active');
+		console.log(homeslideindex);
+	}
+
+function autoslides(){
+	nexthomeSlide();
+}
+const homeslideindicators = document.querySelector('.homeslideindicators');
+for(let k=0; k<homeslider.length; k++){
+	const div = document.createElement('div');
+	div.id = k;
+	homeslideindicators.appendChild(div);
+	div.setAttribute('onclick','thisIndicator(this)');
+	if(k==0){
+		div.classList.add('active');
+	}
+
+
+}
+function thisIndicator(element){
+	homeslideindex = element.id;
+	updateCircle();
+	changehomeSlider();
+}
+function updateCircle(){
+	for(let x=0; x<homeslideindicators.children.length; x++){
+		homeslideindicators.children[x].classList.remove('active');	
+	}
+	homeslideindicators.children[homeslideindex].classList.add('active');
+}
+let homesliderInterval = setInterval(autoslides,5000);
